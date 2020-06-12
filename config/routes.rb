6fa026cb9324e5f03d3bf22a907fdb15327289e7
Neil Daly <neil.daly@wpengine.com>
2020-06-12 15:23:38 +0100

    adding initial login & registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+
+  # call to our cookie sessions
+  resources :sessions, only: %i[create]
+  resources :registrations, only: %i[create]
 end
