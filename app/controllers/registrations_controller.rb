24dfc1928dc594f7296df1652c1916d05a18702f
Neil Daly <neil.daly@wpengine.com>
2020-06-12 15:55:55 +0100

    rubocop registration#create ABC fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,17 +2,21 @@
 
 class RegistrationsController < ApplicationController
   def create
-    user = User.create!(
-      email: params['user']['email'],
-      password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
-    )
+    user = attempt_user_creation
 
     if user
       session[:user_id] = user.id
       render json: { status: :created, user: user }
     end
   rescue ActiveRecord::RecordInvalid => e
-    render json: { status: 422, message: e.record.errors }
+    render json: { status: 400, message: e.record.errors }
+  end
+
+  def attempt_user_creation
+    User.create!(
+      email: params['user']['email'],
+      password: params['user']['password'],
+      password_confirmation: params['user']['password_confirmation']
+    )
   end
 end
