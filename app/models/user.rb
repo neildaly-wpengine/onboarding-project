7b2a4e0b73d8f1538394a5f530379ae4bc78e957
Neil Daly <neil.daly@wpengine.com>
2020-06-12 17:08:56 +0100

    user model testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,13 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   has_secure_password
 
   validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
   validates_uniqueness_of :email
+
+  validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,14 +14,39 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation testing' do
-    it 'ensures email presence' do
+    it 'does not save without an email' do
       user = User.new(password: '123', password_confirmation: '123').save
       expect(user).to eq(false)
     end
 
-    it 'verifies email presence' do
+    it 'saves a valid user' do
       user = User.new(email: '1@1.com', password: '123', password_confirmation: '123').save
       expect(user).to eq(true)
+    end
+
+    it 'does not save without a password' do
+      user = User.new(email: '1@1.com', password_confirmation: '123').save
+      expect(user).to eq(false)
+    end
+
+    it 'does not save without a password confirmation' do
+      user = User.new(email: '1@1.com', password: '123').save
+      expect(user).to eq(false)
+    end
+
+    it 'does not save with unequal passwords' do
+      user = User.new(email: 'd@d.dd', password: 'dd', password_confirmation: 'd').save
+      expect(user).to eq(false)
+    end
+
+    it 'does not save with invalid email regex' do
+      user = User.new(email: 'd@d', password: 'dd', password_confirmation: 'd').save
+      expect(user).to eq(false)
+    end
+
+    it 'does not save with long email' do
+      user = User.new(email: "#{'a' * 255}@a.com", password: 'dd', password_confirmation: 'dd').save
+      expect(user).to eq(false)
     end
   end
 end
