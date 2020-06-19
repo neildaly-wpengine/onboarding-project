61800a6d84e841c55f855db7508c6aeb5eab523e
Neil Daly <neil.daly@wpengine.com>
2020-06-19 14:48:16 +0100

    standardizing user creation with user params

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,26 +2,21 @@ module Api
   module V1
     class RegistrationsController < ApplicationController
       def create
-        user = attempt_user_creation(params['user'])
+        user = User.new(user_params)
 
-        if user
+        if user.save
           session[:user_id] = user.id
           render json: { status: :created, user: user }
+        else
+          render json: { status: 400, message: user.errors.messages }
         end
-      rescue ActiveRecord::RecordInvalid => e
-        render json: { status: 400, message: e.record.errors }
       end
 
       private
 
-      def attempt_user_creation(user)
-        User.create!(
-          email: user['email'],
-          first_name: user['first_name'],
-          last_name: user['last_name'],
-          password: user['password'],
-          password_confirmation: user['password_confirmation']
-        )
+      # Only allow a list of trusted parameters through.
+      def user_params
+        params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
       end
     end
   end
