e65f3ad2496bb10cc759a04a3b4a6f0c3b502afd
Neil Daly <neil.daly@wpengine.com>
2020-06-24 09:29:11 +0100

    initial articles fetching

--- app/javascript/components/articles/ArticleList.tsx
+++ app/javascript/components/articles/ArticleList.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Article {
+  attributes: ArticleAttributes;
+  id: number;
+  user: User;
+}
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  initialsAvatar: string;
+}
+
+interface ArticleAttributes {
+  title: string;
+  content: string;
+  userID: number;
+  createdAt: Date;
+  updatedAt: Date;
+  discardedAt?: Date;
+  archived: boolean;
+}
 
 const ArticleList: React.FC = () => {
-  return <div>Articles</div>;
+  const [articles, setArticles] = useState<Article[]>([]);
+
+  useEffect(() => {
+    const fetchAllArticles = async () => {
+      const articlesResponse = await axios.get("/api/v1/articles");
+      setArticles(articlesResponse.data.data);
+    };
+
+    fetchAllArticles();
+  }, []);
+
+  if (articles === []) {
+    return null;
+  }
+
+  console.log(articles);
+
+  const articlesList = articles.map((article: Article, index: number) => {
+    return (
+      <li key={index}>
+        {article.attributes.title} -{" "}
+        {article.attributes.content.substring(0, 20)}
+      </li>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <h1>Articles</h1>
+      <ul>{articlesList}</ul>
+    </React.Fragment>
+  );
 };
 
 export default ArticleList;
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
-  get '*path', to: 'pages#index'
 
   namespace :api do
     namespace :v1 do
@@ -19,4 +18,5 @@ Rails.application.routes.draw do
       end
     end
   end
+  match '*path', to: 'pages#index', via: :all
 end
