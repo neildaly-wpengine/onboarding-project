dd06e68699c01938090077d6a8765c2b2715de52
Neil Daly <neil.daly@wpengine.com>
2020-06-29 11:40:03 +0100

    create register route

---
+++ app/javascript/components/auth/Registration.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ConsumerProps } from "../../common/types";
+
+const Registration: React.FC<ConsumerProps> = () => {
+  return <div>Registration</div>;
+};
+
+export default Registration;
