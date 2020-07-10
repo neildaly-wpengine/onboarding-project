2c87820f3c87e6ca93b40ba93676e0c3f146bc42
Neil Daly <neil.daly@wpengine.com>
2020-07-10 12:02:05 +0100

    add heroku origin whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://maple-toque-44070.herokuapp.com/'
 
     resource '*',
              headers: :any,
