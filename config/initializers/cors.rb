70e036bbf9eee943e1df53b9a6c425200caacc4f
Neil Daly <neil.daly@wpengine.com>
2020-07-10 14:05:47 +0100

    whitelist bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://maple-toque-44070.herokuapp.com/'
+    origins 'https://maple-toque-44070.herokuapp.com'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
